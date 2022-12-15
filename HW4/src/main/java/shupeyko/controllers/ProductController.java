b10d39545df87372eb3b05061b60419982a042c7
Andrey Shupeyko <andryws@mail.ru>
2022-12-15 15:43:47 +0300

    Spring HomeWork 4

---
+++ HW4/src/main/java/shupeyko/controllers/ProductController.java
@@ -0,0 +1,49 @@
+package shupeyko.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import shupeyko.services.ProductService;
+import shupeyko.model.Product;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    ProductService productService;
+    Logger logger = LoggerFactory.getLogger(ProductController.class);
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+//    @GetMapping("/products")
+    @GetMapping
+    public List<Product> getAllProducts(){
+        logger.info("get");
+        return productService.getAllProducts();
+    }
+
+//    @DeleteMapping("/products/{id}")
+    @DeleteMapping("/{id}")
+    public void deleteProductById(@PathVariable("id") Long id){
+        productService.deleteById(id);
+    }
+
+//    @PutMapping("/products")
+    @PutMapping
+    public void changePrice( @RequestParam("delta") Integer delta, @RequestParam(name = "productId") Long id){
+        productService.changePrice(id, delta);
+    }
+
+//    @PostMapping("/products")
+    @PostMapping
+    public void addProduct(@RequestBody Product product){
+            productService.addProduct(product);
+    }
+
+}
---
+++ HW4/src/test/java/shupeyko/HomeWork4Tests.java
@@ -0,0 +1,13 @@
+package shupeyko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork4Tests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
